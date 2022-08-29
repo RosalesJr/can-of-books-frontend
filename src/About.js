260f21ee456d6fdc0cfc4116e16a71f57f41894c
Adrian Butler Jr <butlerjr123@gmail.com>
2022-08-29 18:17:17 -0500

    added live book info from api

--- src/About.js
+++ src/About.js
@@ -4,7 +4,12 @@ class Profile extends Component {
 
   render() {
     /* TODO: render information about the developers */
-    return <p>Profile page coming soon</p>
+    return (
+        <>
+        <p>Adrian</p>
+        <p>Luis</p>
+        </>
+    )
   }
 };
 
