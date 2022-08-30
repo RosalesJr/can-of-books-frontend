dfcf51d322f7496d986059ee39b552e479249abc
LUIS I ROSALES JR <luis.rosalesjr01@gmail.com>
2022-08-30 15:50:47 -0600

    refactored code to add delete button

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from "axios";
-import {Carousel} from "react-bootstrap";
+import {Button, Carousel} from "react-bootstrap";
+import BookForm from "./BookForm"
 
 class BestBooks extends React.Component
 {
@@ -8,8 +9,29 @@ class BestBooks extends React.Component
 	{
 		super(props);
 		this.state = {
-			books: []
+			books: [],
+			show: false,
 		};
+	}
+
+
+	addBooks = async (event) => {
+		event.preventDefault()
+		this.hideModal()
+		let book = {
+			title: event.target.title.value,
+			description: event.target.description.value,
+			status: event.target.read.checked,
+		}
+		try {
+			let response = await axios.post(`${process.env.REACT_APP_SERVER}/books`, book)
+			let newBook = response.data
+			this.setState({
+				books: [...this.state.books, newBook]
+			});			
+		} catch (error) {
+			console.log('error posting', error)
+		}
 	}
 
 	getBooks = async () =>
@@ -31,11 +53,39 @@ class BestBooks extends React.Component
 		this.getBooks();
 	}
 
+	showModal = () =>{
+		this.setState({
+			show:true
+		})
+	}
+
+	hideModal = () =>{
+		this.setState({
+			show:false
+		})
+	}
+	deleteBook = async (bookId) => {
+		try {
+			let response = await axios.delete(`${process.env.REACT_APP_SERVER}/books/${bookId}`);
+			let newBooks = this.state.books.filter((value,index)=>{
+				if(value._id=== bookId){
+					return false;
+				}else{return true;}
+				})
+			this.setState({
+				books: newBooks,
+			})
+		} catch (error) {
+			console.log('error deleting', error)
+		}
+	
+	}
+
 	render()
 	{
 		console.log(this.state.books);
 		let books = this.state.books.map((value, index) =>
-		{
+		{ 
 		    return (
 			    <Carousel.Item bg-dark>
 				    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/A_black_image.jpg/640px-A_black_image.jpg"
@@ -45,6 +95,7 @@ class BestBooks extends React.Component
 					    <h3>{value.title}</h3>
 					    <p>{value.description}</p>
 					    <p>{value.status}</p>
+							<Button onClick={()=> this.deleteBook(value._id)}>Delete</Button>
 				    </Carousel.Caption>
 			    </Carousel.Item>
 		    )
@@ -54,7 +105,8 @@ class BestBooks extends React.Component
 		return (
 			<>
 				<h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
-
+				<Button onClick={this.showModal}>Add a book</Button>
+					<BookForm addBooks={this.addBooks} show={this.state.show} onHide={this.hideModal}/>
 				{this.state.books.length ? (
 					<Carousel className="w-50">
 						{books}
